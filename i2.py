c53c3875ae7cac5702ad05c6c3f3e0c2892ddb0b
youngmin-jin <youngmin7854@gmail.com>
2023-10-15 08:53:53 +0900

    completed

--- i2.py
+++ i2.py
@@ -5,7 +5,9 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
-from kerastuner.tuners import RandomSearch
+import keras_tuner
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
 # -------------------------------
 # read and modify data
@@ -52,75 +54,80 @@ test_ds = test_ds.prefetch(tf.data.AUTOTUNE)
 # -------------------------------
 # i2 
 # -------------------------------
-print("------------------ model------------------")
-
-# create a model
 def i2_create_model(hp):
   inputs = keras.Input(shape=image_size+(3,))       
   
-  # ------------------------ legacy -----------------------------------------------
-  # x = layers.Rescaling(1.0/255)(inputs)                         # rescaling the RGB channels
-  # x = layers.Conv2D(hp.Choice('neurons', values=[100,500,1000]), 3, strides=2, padding='same')(x)
-  # x = layers.BatchNormalization()(x)
-  # x = layers.Activation(hp.Choice('activation', values=['relu','sigmoid','tanh']))(x)
-                    
-  # previous_block_activation = x
-  
-  # x = layers.Activation(hp.Choice('activation', values=['relu','sigmoid','tanh']))(x)
-  # x = layers.SeparableConv2D(hp.Choice('neurons', values=[100,500,1000]), 3, padding="same")(x)
-  # x = layers.BatchNormalization()(x)
-
-  # x = layers.Activation(hp.Choice('activation', values=['relu','sigmoid','tanh']))(x)
-  # x = layers.SeparableConv2D(hp.Choice('neurons', values=[100,500,1000]), 3, padding="same")(x)
-  # x = layers.BatchNormalization()(x)
-
-  # x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
-
-  # Project residual
-  # residual = keras.layers.Conv2D(hp.Choice('neurons', values=[100,500,1000]), 1, strides=2, padding="same")(
-  #   previous_block_activation
-  # )
-  # x = keras.layers.add([x, residual])  # Add back residual
-  # previous_block_activation = x  # Set aside next residual
-
-  # x = keras.layers.GlobalAveragePooling2D()(x)
-  # x = keras.layers.Dropout(hp.Choice('dropout',values=[0.0,0.2,0.5]))(x)      
-  # -------------------------------------------------------------------------------
-  
-  x = layers.Conv2D(hp.Choice('neuron', values=[100,500,1000]), 3, strides=2, padding='same', activation=hp.Choice('activation', values=['relu','sigmoid','tanh']))(inputs)
+  x = layers.Conv2D(hp.Choice('neuron', values=[100,500,1000,1500,2000]), 3, strides=2, padding='same', activation=hp.Choice('activation', values=['relu','sigmoid','tanh']))(inputs)
   for i in range(hp.Int('hidden_layers', 0, 2)):
-    x = layers.Conv2D(hp.Choice('neuron', values=[100,500,1000])
+    x = layers.Conv2D(hp.Choice('neuron', values=[100,500,1000,1500,2000])
         , 3, strides=2, padding='same'
         , activation=hp.Choice('activation', values=['relu','sigmoid','tanh']))(x)
-  x = layers.SeparableConv2D(hp.Choice('neuron', values=[100,500,1000]), 3, padding="same", activation=hp.Choice('activation', values=['relu','sigmoid','tanh']))(x)
+  x = layers.SeparableConv2D(hp.Choice('neuron', values=[100,500,1000,1500,2000]), 3, padding="same", activation=hp.Choice('activation', values=['relu','sigmoid','tanh']))(x)
   x = layers.GlobalMaxPooling2D()(x)
   x = layers.Dropout(hp.Choice('dropout', values=[0.0,0.2,0.5]))(x)
    
   outputs = keras.layers.Dense(4, activation='softmax')(x)  
   model = keras.Model(inputs, outputs)
-  model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy',keras.metrics.Precision(), keras.metrics.Recall()])
+  model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
   return model
 
 # random search
-i2_model = RandomSearch(
+i2_model = keras_tuner.GridSearch(
   i2_create_model
-  , objective='val_accuracy'
-  , max_trials=1000
+  , objective='accuracy'
   , overwrite=True
-  , directory='i2_tuner1'
-  , project_name='i2_tuner1'
 )
 
-# fit
-num_epochs = 300
-i2_model.search(train_ds, validation_data=test_ds, epochs=num_epochs)
+# search
+num_epochs = 100
+i2_model.search(train_ds, epochs=num_epochs)
 
-# summary 
-print("---------------- results --------------------")
-print(i2_model.results_summary())
-print(i2_model.get_best_models()[0])
+# best model results
+print("---------------- best params --------------------")
+i2_best_param = i2_model.get_best_hyperparameters(num_trials=1)[0]
+print("neuron: ", i2_best_param.get("neuron"))
+print("activation: ", i2_best_param.get("activation"))
+print("hidden_layers: ", i2_best_param.get("hidden_layers"))
+print("dropout: ", i2_best_param.get("dropout"))
 
+print("---------------- best model results --------------------")
+i2_best_model = i2_model.get_best_models(1)[0]
+i2_best_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+print(i2_best_model.summary())
 
+# fit using the best model
+i2_best_model.fit(train_ds)
+
+# evaluate on the test dataset
+print('----------- Evaluation on Test Dataset ---------------')
+test_loss, test_accuracy = i2_best_model.evaluate(test_ds)
+print(f'Test Loss: {test_loss:.4f}')
+print(f'Test Accuracy: {test_accuracy:.4f}')
+
+# predict/ actual and predict values
+y_actual = []
+y_predict = []
+for img, label in test_ds:
+  y_actual.append(label.numpy())
+  y_predict.append(i2_best_model.predict(img).argmax(axis=-1))
+  
+y_actual = np.concatenate(y_actual, axis=0)
+y_actual = np.argmax(np.array(y_actual), axis=1)
+y_predict = np.concatenate(y_predict, axis=0)
+
+print("----------------- y_actual ----------------------")
+print(y_actual)
+
+print("----------------- y_predict ----------------------")
+print(y_predict)
+
+# confusion matrix
+print("----------------- confusion matrix ----------------------")
+print(confusion_matrix(y_actual, y_predict))
+
+# confusion report
+print("----------------- confusion report ----------------------")
+print(classification_report(y_actual, y_predict))
 
 
 
--- s2.py
+++ s2.py
@@ -36,27 +36,27 @@ ks_x_train, ks_x_test, ks_y_train, ks_y_test = train_test_split(df[predictors], 
 # build a sequential model
 def s2_create_model(hp):
   model = keras.models.Sequential()
-  model.add(keras.layers.Dense(hp.Choice('neuron', values=[100,500,1000])
-  , input_shape=(df[predictors].shape[1],)
+  model.add(keras.layers.Dense(hp.Choice('neuron', values=[100,500,1000,1500,2000])
+  , input_shape=(ks_x_train.shape[1],)
   , activation=hp.Choice('activation', values=['relu','sigmoid','tanh'])))
   for i in range(hp.Int('hidden_layers', 2, 4)):
-    model.add(keras.layers.Dense(hp.Choice('neuron', values=[100,500,1000])
+    model.add(keras.layers.Dense(hp.Choice('neuron', values=[100,500,1000,1500,2000])
     , activation=hp.Choice('activation', values=['relu','sigmoid','tanh'])))
   model.add(keras.layers.Dropout(hp.Choice('dropout', values=[0.0,0.2,0.5])))
   model.add(keras.layers.Dense(4, activation='softmax'))
-  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy',keras.metrics.Precision(), keras.metrics.Recall()])
+  model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
   return model
 
 # make a model
 s2_model = keras_tuner.GridSearch(
   s2_create_model
-  , objective='val_accuracy'
+  , objective='accuracy'
   , overwrite=True
 )
 
 # search
-num_epochs = 300
-s2_model.search(ks_x_train, ks_y_train, validation_data=(ks_x_test, ks_y_test), epochs=num_epochs)
+num_epochs = 100
+s2_model.search(ks_x_train, ks_y_train, epochs=num_epochs)
 
 # best model results
 print("---------------- best params --------------------")
@@ -72,7 +72,13 @@ s2_best_model.build(ks_x_train.shape)
 print(s2_best_model.summary())
 
 # fit using the best model
-s2_best_model.fit(ks_x_train, ks_y_train, epochs=num_epochs)
+s2_best_model.fit(ks_x_train, ks_y_train)
+
+# evaluate on the test dataset
+print('----------- Evaluation on Test Dataset ---------------')
+test_loss, test_accuracy = s2_best_model.evaluate(ks_x_test)
+print(f'Test Loss: {test_loss:.4f}')
+print(f'Test Accuracy: {test_accuracy:.4f}')
 
 # actual and predicted value
 print("----------------- y_actual ----------------------")
