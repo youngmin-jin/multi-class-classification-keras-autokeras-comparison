db81df7713fac2a61d0c475b51304a913b6bd03b
Judita Preiss <judita.preiss@sheffield.ac.uk>
2023-11-09 23:52:15 +0000

    Avoiding one-hot encoding

--- t2-b.py
+++ t2-b.py
@@ -31,29 +31,46 @@ print('keras_nlp', keras_nlp.__version__)
 # -------------------------------
 # read the data
 df = pd.read_csv('text-test.csv', encoding='utf-8')
-df_original = df.copy()
 
-# one hot encoding
-dummies = pd.get_dummies(df['Sentiment'])
-df.drop('Sentiment', axis=1, inplace=True)
-df = pd.concat([df, dummies], axis=1)
+# convert sentiment to numeric value
+def convert_sentiment(value):
+  if value == 'positive':
+    return 2
+  elif value == 'neutral':
+    return 1
+  else:
+    assert value == 'negative'
+    return 0
 
-# split into training and test datasets
-num = df.shape[0] * 0.7
-num = round(num)
-df_train = df.loc[:num]
-df_test = df.loc[num+1:]
+df['numeric_sentiment'] = df.apply(lambda x: convert_sentiment(x['Sentiment']), axis=1)  
 
-# Split into features and target
-target = ['negative', 'neutral', 'positive']
-# x = df['Sentence']
-# y = df[target]
+# create train test split
+x_trainDF, x_testDF, y_trainDF, y_testDF = train_test_split(df[['Sentence']], df[['numeric_sentiment']], test_size= 0.3)
+
+
+#df_original = df.copy()
+
+## one hot encoding
+#dummies = pd.get_dummies(df['Sentiment'])
+#df.drop('Sentiment', axis=1, inplace=True)
+#df = pd.concat([df, dummies], axis=1)
+
+## split into training and test datasets
+#num = df.shape[0] * 0.7
+#num = round(num)
+#df_train = df.loc[:num]
+#df_test = df.loc[num+1:]
+
+## Split into features and target
+#target = ['negative', 'neutral', 'positive']
+## x = df['Sentence']
+## y = df[target]
 
 # Split into training and test sets
 # x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7)
 
-raw_train_ds = tf.data.Dataset.from_tensor_slices((df_train['Sentence'], df_train[target])).batch(batch_size=32)
-raw_test_ds = tf.data.Dataset.from_tensor_slices((df_test['Sentence'], df_test[target])).batch(batch_size=32)
+#raw_train_ds = tf.data.Dataset.from_tensor_slices((df_train['Sentence'], df_train[target])).batch(batch_size=32)
+#raw_test_ds = tf.data.Dataset.from_tensor_slices((df_test['Sentence'], df_test[target])).batch(batch_size=32)
 
 # prepare the setting
 # max_features = 20000
@@ -90,12 +107,18 @@ t2_b_model = keras_nlp.models.BertClassifier.from_preset(
   , num_classes=3
 )
 
+# without one-hot encoding (but numeric sentiment)
+t2_b_model.compile(loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True), optimizer='adam', metrics=['accuracy'])
+
 # compile
-t2_b_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#t2_b_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # fit
 num_epochs = 5
-t2_b_model.fit(raw_train_ds, epochs=num_epochs)
+# without one-hot encoding
+t2_b_model.fit(x=x_trainDF['Sentence'].tolist(), y=y_trainDF['numeric_sentiment'].tolist(), epochs=num_epochs)
+
+#t2_b_model.fit(raw_train_ds, epochs=num_epochs)
 
 # summary
 print('----------- results ---------------')
@@ -113,14 +136,18 @@ print(t2_b_model.summary())
 # print(y_predict)
 
 # predict/ actual and predict values
-y_actual = []
 y_predict = []
-for text, label in raw_test_ds:
-  y_actual.append(label.numpy())
-  y_predict.append(t2_b_model.predict(text).argmax(axis=-1))
+# without one-hot encoding
+for text in x_testDF['Sentence'].tolist():
+  y_predict.append(t2_b_model.predict([text]).argmax(axis=-1))
+#y_actual = []
+#for text, label in raw_test_ds:
+#  y_actual.append(label.numpy())
+#  y_predict.append(t2_b_model.predict(text).argmax(axis=-1))
   
-y_actual = np.concatenate(y_actual, axis=0)
-y_actual = np.argmax(np.array(y_actual), axis=1)
+#y_actual = np.concatenate(y_actual, axis=0)
+#y_actual = np.argmax(np.array(y_actual), axis=1)
+y_actual = y_testDF['numeric_sentiment'].to_numpy()
 y_predict = np.concatenate(y_predict, axis=0)
 
 print("----------------- y_actual ----------------------")
