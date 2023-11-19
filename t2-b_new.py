053326f2faef9837deb87ec7cf43cc4a4e9ec980
youngmin-jin <youngmin7854@gmail.com>
2023-11-19 16:38:34 +0900

    revert to Avoiding one-hot encoding

--- t2-b_new.py
+++ t2-b_new.py
@@ -12,6 +12,7 @@ from sklearn.metrics import confusion_matrix
 from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
 import keras_nlp
+from tensorflow.keras.optimizers import Adam
 
 # -------------------------------
 # version check
@@ -20,6 +21,7 @@ import keras_nlp
 
 print('tensorflow', tf.__version__)
 print('keras', keras.__version__)
+print('keras_tuner', keras_tuner.__version__)
 print('keras_nlp', keras_nlp.__version__)
 
 # -------------------------------
@@ -46,33 +48,55 @@ target = ['negative', 'neutral', 'positive']
 raw_train_ds = tf.data.Dataset.from_tensor_slices((df_train['Sentence'], df_train[target])).batch(batch_size=32)
 raw_test_ds = tf.data.Dataset.from_tensor_slices((df_test['Sentence'], df_test[target])).batch(batch_size=32)
 
+# do async prefetching
+raw_train_ds = raw_train_ds.cache().prefetch(buffer_size=10)
+raw_test_ds = raw_test_ds.cache().prefetch(buffer_size=10)
+
 # ---------------------------------------
 # t2-b
 # ---------------------------------------
-# BertClassifier
-t2_b_model = keras_nlp.models.BertClassifier.from_preset(
-  "bert_tiny_en_uncased"
-  , num_classes=3
-  , activation='softmax'
+# BertClassifier model
+def t2_b_create_model(hp):
+  model = keras_nlp.models.BertClassifier.from_preset(
+    "bert_base_en_uncased"
+    , num_classes=3
+    , activation='softmax'
+    , dropout=hp.Choice("dropout", values=[0.0,0.2,0.5])
+)
+  model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=hp.Choice("learning_rate", values=[0.01, 0.001, 0.0001, 0.00001])), metrics=['accuracy'])
+  return model
+
+# grid search 
+t2_b_model = keras_tuner.GridSearch(
+  t2_b_create_model
+  , objective='accuracy'
+  , overwrite=True
 )
 
-# compile
-t2_b_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# fit
+# search
 num_epochs = 100
-t2_b_model.fit(raw_train_ds, epochs=num_epochs)
+t2_b_model.search(raw_train_ds, epochs=num_epochs)
 
-# summary
-print('----------- results ---------------')
-print(t2_b_model.summary())
+# best model results
+print("---------------- best params --------------------")
+t2_b_best_param = t2_b_model.get_best_hyperparameters(num_trials=1)[0]
+print("learning_rate: ", t2_b_best_param.get("learning_rate"))
+print("dropout: ", t2_b_best_param.get("dropout"))
+
+print("---------------- best model results --------------------")
+t2_b_best_model = t2_b_model.get_best_models()[0]
+t2_b_best_model.build(raw_train_ds)
+print(t2_b_best_model.summary())
+
+# fit using the best model
+t2_b_best_model.fit(raw_train_ds)
 
 # predict/ actual and predict values
 y_actual = []
 y_predict = []
 for text, label in raw_test_ds:
   y_actual.append(label.numpy())
-  y_predict.append(t2_b_model.predict(text).argmax(axis=-1))
+  y_predict.append(t2_b_best_model.predict(text).argmax(axis=-1))
   
 y_actual = np.concatenate(y_actual, axis=0)
 y_actual = np.argmax(np.array(y_actual), axis=1)
