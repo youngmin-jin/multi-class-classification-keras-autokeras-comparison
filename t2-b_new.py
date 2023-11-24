235d6ccfa61609219295aaa9f0f1590623c49af0
youngmin-jin <youngmin7854@gmail.com>
2023-11-24 15:48:42 +0900

    base_large_en_uncased, dropout, LR, batch size applied

--- t2-b_new.py
+++ t2-b_new.py
@@ -1,4 +1,4 @@
-# libraries 
+# libraries
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -36,76 +36,63 @@ dummies = pd.get_dummies(df['Sentiment'])
 df.drop('Sentiment', axis=1, inplace=True)
 df = pd.concat([df, dummies], axis=1)
 
-# split into training and test datasets
-num = df.shape[0] * 0.7
-num = round(num)
-df_train = df.loc[:num]
-df_test = df.loc[num+1:]
-
 # convert df to tf.dataset with batch 32
 target = ['negative', 'neutral', 'positive']
 
-raw_train_ds = tf.data.Dataset.from_tensor_slices((df_train['Sentence'], df_train[target])).batch(batch_size=32)
-raw_test_ds = tf.data.Dataset.from_tensor_slices((df_test['Sentence'], df_test[target])).batch(batch_size=32)
-
-# do async prefetching
-raw_train_ds = raw_train_ds.cache().prefetch(buffer_size=10)
-raw_test_ds = raw_test_ds.cache().prefetch(buffer_size=10)
+# split
+x_train, x_test, y_train, y_test = train_test_split(df['Sentence'], df[target], train_size=0.7)
 
 # ---------------------------------------
 # t2-b
 # ---------------------------------------
-# BertClassifier model
-def t2_b_create_model(hp):
-  model = keras_nlp.models.BertClassifier.from_preset(
-    "bert_base_en_uncased"
-    , num_classes=3
-    , activation='softmax'
-    , dropout=hp.Choice("dropout", values=[0.0,0.2,0.5])
-)
-  model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=hp.Choice("learning_rate", values=[0.01, 0.001, 0.0001, 0.00001])), metrics=['accuracy'])
-  return model
+# BertClassifier model 
+class t2_b_create_model(keras_tuner.HyperModel):
+  def build(self, hp):
+    model = keras_nlp.models.BertClassifier.from_preset(
+      "bert_large_en_uncased"
+      , num_classes=3
+      , activation='softmax'
+      , dropout=hp.Choice("dropout", values=[0.0,0.2,0.5])
+  )
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(learning_rate=hp.Choice("learning_rate", values=[3e-4, 1e-4, 5e-5, 3e-5])), metrics=['accuracy'])
+    return model
+
+  def fit(self, hp, model, *args, **kwargs):
+    return model.fit(*args, batch_size=hp.Choice("batch_size", values=[8, 16, 32, 64, 128]), **kwargs)
 
 # grid search 
 t2_b_model = keras_tuner.GridSearch(
-  t2_b_create_model
+  t2_b_create_model()
   , objective='accuracy'
   , overwrite=True
 )
 
 # search
-num_epochs = 100
-t2_b_model.search(raw_train_ds, epochs=num_epochs)
+num_epochs = 5
+t2_b_model.search(x_train, y_train, epochs=num_epochs)
 
 # best model results
 print("---------------- best params --------------------")
 t2_b_best_param = t2_b_model.get_best_hyperparameters(num_trials=1)[0]
 print("learning_rate: ", t2_b_best_param.get("learning_rate"))
 print("dropout: ", t2_b_best_param.get("dropout"))
+print("batch_size: ", t2_b_best_param.get("batch_size"))
 
 print("---------------- best model results --------------------")
 t2_b_best_model = t2_b_model.get_best_models()[0]
-t2_b_best_model.build(raw_train_ds)
 print(t2_b_best_model.summary())
 
 # fit using the best model
-t2_b_best_model.fit(raw_train_ds)
+t2_b_best_model.fit(x_train, y_train)
 
 # predict/ actual and predict values
-y_actual = []
-y_predict = []
-for text, label in raw_test_ds:
-  y_actual.append(label.numpy())
-  y_predict.append(t2_b_best_model.predict(text).argmax(axis=-1))
-  
-y_actual = np.concatenate(y_actual, axis=0)
-y_actual = np.argmax(np.array(y_actual), axis=1)
-y_predict = np.concatenate(y_predict, axis=0)
-
 print("----------------- y_actual ----------------------")
+y_actual = np.argmax(np.array(y_test), axis=1)
 print(y_actual)
 
 print("----------------- y_predict ----------------------")
+y_predict = t2_b_best_model.predict(x_test)
+y_predict = y_predict.argmax(axis=-1)
 print(y_predict)
 
 # confusion matrix
