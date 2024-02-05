1638ca94a12903b05948896515286c4b3bb86470
youngmin-jin <youngmin7854@gmail.com>
2024-02-05 22:51:57 +0900

    being finetuned

--- Io2.py
+++ Io2.py
@@ -8,7 +8,7 @@ from input_output import *
 from tensorflow.keras.applications.xception import preprocess_input
 
 # -------------------------------
-# version check 
+# version check
 # -------------------------------
 print('tensorflow', tf.__version__)
 print('keras', keras.__version__)
@@ -29,17 +29,17 @@ def Io2_create_model(hp):
   inputs = keras.Input(shape=image_size+(3,)) 
 
   # hidden layers
-  x = keras.layers.BatchNormalization()(inputs)
-  # x = keras.layers.RandomFlip('horizontal')(x)
-  # x = keras.layers.RandomContrast(0.1)(x)
+  x = keras.layers.RandomFlip(hp.Choice("random_flip", values=['vertical','horizontal','horizontal_and_vertical']))(inputs)
+  x = keras.layers.RandomContrast(hp.Choice("random_contrast", values=[0.1,0.5]))(x)
+  x = keras.layers.BatchNormalization(momentum=hp.Choice("momentum", values=[0.5,0.99]))(x)
   x = keras.layers.Resizing(224,224)(x)
   x = keras.applications.Xception(
       include_top=False,
       weights='imagenet',
       pooling=hp.Choice("pooling", values=['avg', 'max'])
   )(x)
-  # x = keras.layers.GlobalAveragePooling2D()(x)
-  # x = keras.layers.Dense(200, activation=hp.Choice("activation", values=['relu','sigmoid']))(x)
+#  x = keras.layers.GlobalAveragePooling2D()(x)
+#  x = keras.layers.Dense(200, activation=hp.Choice("activation", values=['relu','sigmoid']))(x)
 
   # output layer
   outputs = keras.layers.Dense(4, activation='softmax')(x)  
@@ -53,15 +53,15 @@ Io2_model = keras_tuner.GridSearch(
   Io2_create_model
   , objective='accuracy'
   , overwrite=True
-  , max_trials=2
+  , max_trials=20
 )
 
 # search
-num_epochs = 3
+num_epochs = 5
 Io2_model.search(x_train, y_train, epochs=num_epochs)
 
 # best parameters
-get_best_params(Io2_model, "pooling")
+get_best_params(Io2_model, "pooling", "random_flip", "random_contrast", "momentum")
 
 # best model summary 
 Io2_best_model = get_best_model(Io2_model, x_train, y_train)
