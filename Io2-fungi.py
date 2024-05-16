7199a5c528f371ec7848cb719401fafd196ff18e
youngmin-jin <youngmin7854@gmail.com>
2024-05-16 10:14:55 +0900

    in progress

--- Io2-fungi.py
+++ Io2-fungi.py
@@ -22,47 +22,54 @@ x_train, x_test, y_train, y_test = create_image_input('fungi',True)
 # -------------------------------
 # Io2
 # -------------------------------
-# def Io2_create_model(hp):
-#   # image_size = (224,224)
+def Io2_create_model(hp):
+  # image_size = (224,224)
   
-#   # input layer
+  # input layer
+  inputs = keras.Input(shape=image_size+(3,)) 
+
+  # hidden layers
+  x = keras.layers.RandomFlip('horizontal')(inputs)
+  x = keras.layers.RandomRotation(hp.Choice("random_rotation", values=[0.005,0.01]))(x)
+  # x = keras.layers.BatchNormalization(momentum=hp.Choice("momentum", values=[0.5,0.99]))(x)
+  x = keras.layers.BatchNormalization()(x)
+  x = keras.layers.Resizing(224,224)(x)
+  x = keras.applications.EfficientNetB7(
+      input_shape=(224,224,3)
+      , include_top=False
+      , weights='imagenet'
+      , drop_connect_rate=hp.Choice("drop_connect_rate", values=[0.005,0.01])
+      # , pooling=hp.Choice("pooling", values=['avg','max'])
+      , pooling='max'
+  )(x)
+  # x = keras.layers.GlobalAveragePooling2D()(x)
+  # x = keras.layers.Dropout(hp.Choice("dropout", values=[0.2,0.5]))(x)
+  x = keras.layers.Dropout(hp.Choice("dropout", values=[0.2,0.5]))(x)
+
+  # output layer
+  outputs = keras.layers.Dense(5, activation='softmax')(x)  
+
+  model = keras.Model(inputs, outputs)
+  model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(learning_rate=hp.Choice("learning_rate", values=[5e-5, 3e-5])), metrics=['accuracy'])
+  # model.compile(loss='categorical_crossentropy', optimizer=keras.optimizers.Adam(learning_rate=3e-5), metrics=['accuracy'])
+  return model
+
+
+# def Io2_create_model(hp):
 #   inputs = keras.Input(shape=image_size+(3,)) 
+#   model = keras.applications.EfficientNetB7(include_top=False, input_tensor=inputs, weights="imagenet")
+  
+#   model.trainable = False
 
-#   # hidden layers
-#   x = keras.layers.RandomFlip(hp.Choice("random_flip", values=['vertical','horizontal','horizontal_and_vertical']))(inputs)
-#   x = keras.layers.RandomRotation(hp.Choice("random_rotation", values=[0.003,0.005,0.01,0.03]))(x)
-#   x = keras.layers.BatchNormalization(momentum=hp.Choice("momentum", values=[0.5,0.99]))(x)
-#   # x = keras.layers.BatchNormalization()(x)
-#   x = keras.layers.Resizing(224,224)(x)
-#   x = keras.applications.EfficientNetB7(
-#       input_shape=(224,224,3)
-#       , include_top=False
-#       , weights='imagenet'
-#       , pooling=hp.Choice("pooling", values=['avg', 'max'])
-#       , drop_connect_rate=hp.Choice("drop_connect_rate", values=[0.1,0.5])
-#   )(x)
-
-#   # output layer
-#   outputs = keras.layers.Dense(5, activation='softmax')(x)  
-
-#   model = keras.Model(inputs, outputs)
-#   model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+#   x = keras.layers.GlobalAveragePooling2D()(model.output)
+#   x = keras.layers.BatchNormalization()(x)
+#   x = keras.layers.Dropout(hp.Choice("dropout", values=[0.2,0.5]))(x)
+#   outputs = keras.layers.Dense(5, activation="softmax")(x)
+  
+#   model = keras.Model(inputs, outputs, name="EfficientNet")
+#   model.compile(loss="categorical_crossentropy", optimizer=keras.optimizers.Adam(learning_rate=hp.Choice("learning_rate", values=[3e-4, 1e-4, 5e-5, 3e-5])), metrics=["accuracy"])
 #   return model
 
-def Io2_create_model(hp):
-  inputs = keras.Input(shape=image_size+(3,)) 
-  model = keras.applications.EfficientNetB7(include_top=False, input_tensor=inputs, weights="imagenet")
-  
-  model.trainable = False
-
-  x = keras.layers.GlobalAveragePooling2D()(model.output)
-  x = keras.layers.BatchNormalization()(x)
-  x = keras.layers.Dropout(hp.Choice("dropout", values=[0.2,0.5]))(x)
-  outputs = keras.layers.Dense(5, activation="softmax")(x)
-  
-  model = keras.Model(inputs, outputs, name="EfficientNet")
-  model.compile(loss="categorical_crossentropy", optimizer=keras.optimizers.Adam(learning_rate=hp.Choice("learning_rate", values=[3e-4, 1e-4, 5e-5, 3e-5])), metrics=["accuracy"])
-  return model
 
 # apply grid search
 Io2_model = keras_tuner.GridSearch(
@@ -84,7 +91,8 @@ Io2_model.search(x_train, y_train, epochs=num_epochs, validation_split=0.2, call
 # Io2_model.search(x_train, y_train, epochs=num_epochs)
 
 # best parameters
-get_best_params(Io2_model, "dropout", "learning_rate")
+get_best_params(Io2_model, "random_flip", "random_rotation", "drop_connect_rate", "dropout", "learning_rate")
+# get_best_params(Io2_model, "random_flip")
 
 # best model summary 
 Io2_best_model = get_best_model(Io2_model, x_train, y_train)
